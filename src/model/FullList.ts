7c6e5c8bdebb5c3d1354c3853fa4d03622bd5152
platisam <platisam@gmail.com>
2023-05-26 22:48:43 +0200

    creating interface List

---
+++ src/model/FullList.ts
@@ -0,0 +1,10 @@
+import ListItem from "./ListItem";
+
+interface List {
+  list: ListItem[];
+  load(): void;
+  save(): void;
+  clearList(): void;
+  addItem(itemObj: ListItem): void;
+  removeItem(id: string): void;
+}
