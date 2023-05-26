cba3409d4adcb0c101f778de13583056974f3b14
platisam <platisam@gmail.com>
2023-05-27 00:07:58 +0200

    created load() method in FullList class

--- src/model/FullList.ts
+++ src/model/FullList.ts
@@ -18,6 +18,23 @@ export default class FullList implements List {
     return this._list;
   }
 
+  load(): void {
+    const storedList: string | null = localStorage.getItem("myList");
+    if (typeof storedList !== "string") return;
+
+    const parsedList: { _id: string; _item: string; _checked: boolean }[] =
+      JSON.parse(storedList);
+
+    parsedList.forEach((itemObj) => {
+      const newListItem = new ListItem(
+        itemObj._id,
+        itemObj._item,
+        itemObj._checked
+      );
+      FullList.instance.addItem(newListItem);
+    });
+  }
+
   save(): void {
     localStorage.setItem("myList", JSON.stringify(this._list));
   }
