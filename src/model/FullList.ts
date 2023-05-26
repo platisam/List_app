f6a0e11fd55f990f58fb920a46e3e3d44b2c4636
platisam <platisam@gmail.com>
2023-05-26 23:27:32 +0200

    added class FullList and created methods

--- src/model/FullList.ts
+++ src/model/FullList.ts
@@ -8,3 +8,31 @@ interface List {
   addItem(itemObj: ListItem): void;
   removeItem(id: string): void;
 }
+
+export default class FullList implements List {
+  static instance: FullList = new FullList();
+
+  private constructor(private _list: ListItem[] = []) {}
+
+  get list(): ListItem[] {
+    return this._list;
+  }
+
+  save(): void {
+    localStorage.setItem("myList", JSON.stringify(this._list));
+  }
+
+  clearList(): void {
+    this._list = [];
+    this.save();
+  }
+
+  addItem(itemObj: ListItem): void {
+    this._list.push(itemObj);
+    this.save();
+  }
+
+  removeItem(id: string): void {
+    this._list = this._list.filter((item) => item.id !== id);
+  }
+}
