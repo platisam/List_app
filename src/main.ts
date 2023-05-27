bf975ca5845e450effd310ba617e807bff4dae15
platisam <platisam@gmail.com>
2023-05-27 22:34:52 +0200

    created initApp function

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,17 @@ import FullList from "./model/FullList";
 import ListItem from "./model/ListItem";
 import ListTemplate from "./templates/ListTemplate";
 
-const initApp = (): void => {};
+const initApp = (): void => {
+  const fullList = FullList.instance;
+  const template = ListTemplate.instance;
+
+  const itemEntryForm = document.getElementById(
+    "itemEntryForm"
+  ) as HTMLFormElement;
+
+  itemEntryForm.addEventListener("submit", (e: SubmitEvent): void => {
+    e.preventDefault();
+  });
+};
 
 document.addEventListener("DOMContentLoaded", initApp);
