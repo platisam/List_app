3df9654e728291f9a1e80c3ec5de2c24c0030566
platisam <platisam@gmail.com>
2023-05-27 22:15:34 +0200

    import folders in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,8 @@
 import "./css/style.css";
+import FullList from "./model/FullList";
+import ListItem from "./model/ListItem";
+import ListTemplate from "./templates/ListTemplate";
+
+const initApp = (): void => {};
+
+document.addEventListener("DOMContentLoaded", initApp);
